a5ed1a53e4a4d1b0f69e44fb9788f6036ea88f7d
Sean K Smith <ssmith2347@gmail.com>
2020-11-12 19:28:01 -0500

    initial commit

---
+++ src/test/java/com/smfg/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.smfg.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+
+@SpringBootTest
+@SpringBootConfiguration
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
